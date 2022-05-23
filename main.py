4014c5fe6631a092d8ff523f587225fa7953ab9b
Baxter Edmonds <101303762+baxteredmonds@users.noreply.github.com>
2022-05-23 00:47:31 +0000

    3rd line

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
-print ("kia ora welcome to my te reo quiz type(continue to start)")
+print ("kia ora welcome to my te reo quiz")
 
-print ("this is how you say_________")
+print ("this is how you say hello how are you in Te reo")
